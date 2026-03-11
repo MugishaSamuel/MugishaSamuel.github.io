6b4137638827a86b8f71afa8ff6b13bef3693e49
MugishaSamuel <mugishasamuel956@gmail.com>
2026-03-11 18:16:08 +0300

    New update number

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4279706295"
+    serviceWorkerVersion: "1361211558"
   }
 });
